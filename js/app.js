7e5e22b69f4f2bbdf90a4cf0e49523b3ffe37fd3
fabianfv <fabianfloresvadell@gmail.com>
2020-06-08 21:58:37 +0200

    añadidos los arrays de colores

---
+++ js/app.js
@@ -0,0 +1,67 @@
+const rojo = [
+  'IndianRed', 'LightCoral', 'Salmon', 'DarkSalmon', 'Crimson',
+  'Red', 'FireBrick', 'DarkRed'
+]
+
+const rosa = [
+  "Pink", "LightPink", "HotPink", "DeepPink", "MediumVioletRed",
+  "PaleVioletRed"
+]
+
+const naranja = [
+  "LightSalmon", "Coral", "Tomato", "OrangeRed", "DarkOrange",
+  "Orange"
+]
+
+const amarillo = [
+  'Gold', 'Yellow', 'LightYellow', 'LemonChiffon',
+  'LightGoldenrodYellow', 'PapayaWhip', 'Moccasin', 'PeachPuff',
+  'PaleGoldenrod', 'Khaki', 'DarkKhaki'
+]
+
+const purpura = [
+  'Lavender', 'Thistle', 'Plum', 'Violet', 'Orchid', 'Fuchsia',
+  'Magenta', 'MediumOrchid', 'MediumPurple', 'RebeccaPurple',
+  'BlueViolet', 'DarkViolet', 'DarkOrchid', 'DarkMagenta',
+  'Purple', 'Indigo', 'SlateBlue', 'DarkSlateBlue', 'MediumSlateBlue'
+]
+
+const verde = [
+  'GreenYellow', 'Chartreuse', 'LawnGreen', 'Lime', 'LimeGreen',
+  'PaleGreen', 'LightGreen', 'MediumSpringGreen', 'SpringGreen',
+  'MediumSeaGreen', 'SeaGreen', 'ForestGreen', 'Green', 'DarkGreen',
+  'YellowGreen', 'OliveDrab', 'Olive', 'DarkOliveGreen',
+  'MediumAquamarine', 'DarkSeaGreen', 'LightSeaGreen', 'DarkCyan', 'Teal'
+]
+
+const azul = [
+  'Aqua', 'Cyan', 'LightCyan', 'PaleTurquoise', 'Aquamarine', 'Turquoise',
+  'MediumTurquoise', 'DarkTurquoise', 'CadetBlue', 'SteelBlue',
+  'LightSteelBlue', 'PowderBlue', 'LightBlue', 'SkyBlue', 'LightSkyBlue',
+  'DeepSkyBlue', 'DodgerBlue', 'CornflowerBlue', 'MediumSlateBlue',
+  'RoyalBlue', 'Blue', 'MediumBlue', 'DarkBlue', 'Navy', 'MidnightBlue'
+]
+  
+const marron = [
+  'Cornsilk', 'BlanchedAlmond', 'Bisque', 'NavajoWhite', 'Wheat',
+  'BurlyWood', 'Tan', 'RosyBrown', 'SandyBrown', 'Goldenrod',
+  'DarkGoldenrod', 'Peru', 'Chocolate', 'SaddleBrown', 'Sienna',
+  'Brown', 'Maroon'
+]
+
+const blanco = [
+  'White', 'Snow', 'HoneyDew', 'MintCream', 'Azure', 'AliceBlue',
+  'GhostWhite', 'WhiteSmoke', 'SeaShell', 'Beige', 'OldLace',
+  'FloralWhite', 'Ivory', 'AntiqueWhite', 'Linen', 'LavenderBlush',
+  'MistyRose'
+]
+
+const negro = [
+  'Gainsboro', 'LightGray', 'Silver', 'DarkGray', 'Gray', 'DimGray',
+  'LightSlateGray', 'SlateGray', 'DarkSlateGray', 'Black'
+]
+
+const colores = [rosa, rojo, naranja, amarillo, purpura, verde, azul, marron, blanco, negro]
+
+
+
